0799d18d61604b0af8924bec3f3e453fc7e4e683
jpclemente <jpclemente91@gmail.com>
2019-04-25 22:54:24 +0200

    redaccion

--- practica1.R
+++ practica1.R
@@ -1,10 +1,15 @@
+knitr::opts_chunk$set(echo = TRUE)
 library(readr)
-library(caret)
 library(dplyr)
 library(Hmisc)
 library(ggplot2)
 library(VIM)
+library(summarytools)
+library(glmnet)
 
+source('transformations.R')
+
+set.seed(12345)
 
 kc_house_data <- read_csv("data/kc_house_data.csv")
 inTraining <- createDataPartition(pull(kc_house_data), p = .7, list = FALSE,times = 1)
@@ -12,148 +17,24 @@ inTraining <- createDataPartition(pull(kc_house_data), p = .7, list = FALSE,time
 house_training <- slice(kc_house_data, inTraining)
 house_testing <- slice(kc_house_data, -inTraining)
 
-# describe(house_training)
-
-# EDA
-# Primer vistazo.
-
-# Eva
-# ID: hay 80 repetidos.
-# price: posible re-escalado.
-# bathrooms: OK. Los americanos son raros
-# waterfront: OK
-# sqft_lot: OK
-# condition: OK
-# sqft_above: Alta correlacion con sqft_living???
-# yr_built: (mirar con cariño)
-# zipcode: OK
-# long: ok
-
-#Ver los factores de cada variable
-str(house_training)
-
-#ver datos faltantes. No falta ninguno 
-aggr_plot <- aggr(house_training[1:10], col=c('navyblue','red'), numbers=TRUE, sortVars=TRUE,
-                  labels=names(house_testing[1:10]), cex.axis=.7, gap=3, 
-                  ylab=c("Histogram of missing data","Pattern"))
-aggr_plot <- aggr(house_training[11:21], col=c('navyblue','red'), numbers=TRUE, sortVars=TRUE,
-                  labels=names(house_testing[11:21]), cex.axis=.7, gap=3, 
-                  ylab=c("Histogram of missing data","Pattern"))
-
-# ID:
-
-#Vemos que hay varios ids repetidos
-describe(house_training$id)
-ids_duplicados <- house_training[which(duplicated(house_training$id)),]
-# View(ids_duplicados)
-
-#Primero ordenamos descendenetemente todas las variables según la fecha para quedarnos con los más actuales (house_training_order)
-#y eliminar los duplicados con una fecha inferior (house_training_WO)
-house_training_order <-  house_training[order(house_training$date, decreasing = TRUE),] 
-house_training_WO <- house_training_order[!duplicated(house_training_order$id), ] 
-# View(house_training_WO)
-
-# - date: 
-
-# price: posible re-escalado.
-table(house_training_WO$price)
-describe(house_training_WO$price)
-ggplot(data = house_training_WO) + geom_density(aes(price))
-ggplot(data = house_training_WO) + geom_density(aes(log10(price)))
-
-# - bedrooms: posible error en 33. Dato muy atípico.
-#     imputar como la media
-
-# bathrooms: OK. Los americanos son raros
-#se han convertido todos los baños que están a 0 en N/A. No se redondea porque según los decimales indican 
-#si es baño, aseo, etc.
-
-house_training_WO$bathrooms <- house_training_WO$bathrooms%>% dplyr::na_if(0)
-summary(house_training_WO$bathrooms)
-# View(house_training_WO)
-
-describe(house_training_WO$bathrooms)
-table(house_training_WO$bathrooms)
-house_training_WO$bathrooms[house_training_WO$bathrooms=='0'] <- NA
-house_training_WO$bathrooms <- factor(house_training_WO$bathrooms)
-ggplot(data = house_training_WO) + geom_density(aes(bathrooms))
-#?? que hacer aqui?
-View(house_training_WO)
+house_training <- transform(house_training)
 
 
-# - floors: OK. Los americanos son raros
+house_training <- na.omit(house_training)
+house_testing <- na.omit(house_testing)
+x_train <- model.matrix(price~.,house_training)[,-1]
+y_train <- house_training$price
 
-# waterfront: OK. ¿que hacer con este?
-describe(house_training_WO$waterfront)
-table(house_training_WO$waterfront)
-hist(house_training_WO$waterfront,col="lightcyan")
-house_training_WO$waterfront = factor(house_training_WO$waterfront)
-levels(house_training_WO$waterfront)
-# - sqft_living: OK
+cv.out <- cv.glmnet(x_train, y_train, alpha=1, nfolds=10, type.measure="mse")
 
-# sqft_lot: OK.
-describe(house_training_WO$sqft_lot)
-table(house_training_WO$sqft_lot)
-ggplot(data = house_training_WO) + geom_density(aes(sqft_lot))
-ggplot(data = house_training_WO) + geom_density(aes(log(sqft_lot)))
+house_testing <- transform(house_testing)
+x_test <- data.matrix(house_testing %>% select(-price))
 
-# - view: OK
+y_predicted_test=predict(cv.out,newx = x_test, type = "response")
+y_real_test = house_testing$price
 
-# condition: OK
-describe(house_training_WO$condition)
-table(house_training_WO$condition)
-ggplot(data = house_training_WO) + geom_density(aes(condition))
-#?? AQUI LINEAL?
-
-# - grade: posibilidad de reducir a 3 valores.
-
-# sqft_above: Alta correlacion con sqft_living?? Esta se puede poner con log o sin el
-describe(house_training_WO$sqft_above)
-table(house_training_WO$sqft_above)
-ggplot(data = house_training_WO) + geom_density(aes(sqft_above))
-
-# - sqft_basement: 0 son no aplica, pues no tiene sentido.
-
-# yr_built: (mirar con cariño). No entiendo qué hay de raro
-describe(house_training_WO$yr_built)
-table(house_training_WO$yr_built)
-ggplot(data = house_training_WO) + geom_density(aes(yr_built))
-
-Old <- house_training_WO[which(house_training_WO$yr_built < 1930),] 
-OldAndNoRenovated <- Old[which(Old$yr_renovated == 0),]
-View(OldAndNoRenovated)
-
-SameSqrt <- OldAndNoRenovated[OldAndNoRenovated$sqft_living == OldAndNoRenovated$sqft_living15,]
-View(SameSqrt)
-
-ggplot(data = SameSqrt) + geom_density(aes(lat))
-ggplot(data = SameSqrt) + geom_density(aes(long))
+y = (y_real_test - y_predicted_test)^2
+mean(y)
 
 
-# - yr_renovated: muchos ceros que son not available.
-
-# zipcode: OK
-describe(house_training_WO$zipcode)
-table(house_training_WO$zipcode)
-ggplot(data = house_training_WO) + geom_density(aes(zipcode))
-
-# - lat: ok
-
-# long: ok
-describe(house_training_WO$long)
-table(house_training_WO$long)
-ggplot(data = house_training_WO) + geom_density(aes(long))
-
-# - sqft_living15: ok
-describe(house_training_WO$sqft_living15)
-table(house_training_WO$sqft_living15)
-ggplot(data = house_training_WO) + geom_density(aes(sqft_living15))
-
-# Javi
-
-# sqft_basement: 0 son no aplica, pues no tiene sentido.
-
-# yr_renovated: muchos ceros que son not available. Demasiados, quizas sea bueno convertir en variable booleana que indique si ha sido renovada.
-table(house_training_WO$yr_renovated)
-house_training_WO$yr_renovated <- house_training_WO$yr_renovated %>% dplyr::na_if(0)
-
+plot(cv.out)
--- transformations.R
+++ transformations.R
@@ -8,37 +8,35 @@ transform <- function(house_df){
   # Set NAs.
   house_df$bathrooms <- house_df$bathrooms %>% dplyr::na_if(0)
   house_df$bedrooms <- house_df$bedrooms %>% dplyr::na_if(33)
-  # house_df$yr_renovated <- house_df$yr_renovated %>% dplyr::na_if(0)
-  # house_df$sqft_basement <- house_df$sqft_basement %>% dplyr::na_if(0)
+  
+  # Binary variables
+  house_df <- house_df %>% mutate(has_basement = if_else(sqft_basement == 0, 0,1))
+  house_df <- house_df %>% mutate(renovated = if_else(yr_renovated == 0, 0,1))
   
   # Impute values
-  house_df <- kNN(house_df, variable = c("bedrooms"), dist_var = c("sqft_living", "floors"), k = 5, imp_var = FALSE)
+  house_df <- kNN(house_df, variable = c("bedrooms"), dist_var = c("sqft_living", "floors", "has_basement"), k = 5, imp_var = FALSE)
   #house_df <- kNN(house_df, variable = c("bathrooms"), dist_var = c("sqft_living", "floors"), k = 5, imp_var = FALSE)
   
-  # transformations
+  # logarithmic transformations
   house_df <- mutate(house_df, price = log(price))
   house_df <- mutate(house_df, sqft_living = log(sqft_living))
   house_df <- mutate(house_df, sqft_lot = log(sqft_lot))
   house_df <- mutate(house_df, sqft_lot15 = log(sqft_lot15))
   
-  # Binary
-  house_df <- house_df %>% mutate(has_basement = if_else(sqft_basement == 0, 0,1))
-  house_df <- house_df %>% mutate(renovated = if_else(yr_renovated == 0, 0,1))
+  #dummies
+  house_df$grade_range <- ifelse(house_df$grade %in% c(1,2,3), 1, 
+                                ifelse(house_df$grade %in% c(4,5,6), 2,
+                                ifelse(house_df$grade %in% c(7,8,9), 3, 4)))  
+  
+  grade <- factor(house_df$grade_range)
+  house_df <- house_df %>% cbind(model.matrix(~grade)[,-1])
+
+  # Impute values
+  house_df <- kNN(house_df, variable = c("bedrooms"), dist_var = c("sqft_living", "floors", "has_basement", "grade2", "grade3", "grade4"), k = 5, imp_var = FALSE)
+  #house_df <- kNN(house_df, variable = c("bathrooms"), dist_var = c("sqft_living", "floors"), k = 5, imp_var = FALSE)
   
   # drop unnecessary variables
-  house_df <- house_df %>% select(-c(id, sqft_basement, yr_renovated))
-  
-  # factores
-  # house_training_WO$bathrooms <- factor(house_training_WO$bathrooms)
-  
-  #dummies
-  house_df$grade_range <- ifelse(house_df$grade %in% c(1,2,3), "Rango1", 
-                                ifelse(house_df$grade %in% c(4,5,6), "Rango2",
-                                ifelse(house_df$grade %in% c(7,8,9), "Rango3", "Rango4")))  
-  
-  fac = factor(house_df$grade_range)
-  grade_matrix = model.matrix(~fac)[,-1]
-  house_df = house_df %>% select(-c("grade"), c(grade_matrix["facRango2"], grade_matrix["facRango3"], grade_matrix["facRango4"]))
+  house_df <- house_df %>% select(-c(id, sqft_basement, yr_renovated, grade, grade_range))
   
   return(house_df)
 }
